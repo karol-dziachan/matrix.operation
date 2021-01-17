2e355069f9471eb8badfcad2564586eaacb4cef2
karol-dziachan <77331159+karol-dziachan@users.noreply.github.com>
2021-01-17 19:16:09 +0100

    Delete matrix directory

--- matrix/const.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-static const int DEFAULT_DIM = 1;
-static const int DEFAULT_ALLOCATE_VALUE = 0;
-static const int DEFAULT_VALUE = 5;
-static const int DEFAULT_VALUE_DOUBLE = 5.0;
-static const int DEFAULT_VALUE_FLOAT = 5.0;
